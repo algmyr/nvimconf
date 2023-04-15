5704a4b588f3fee3d934c9079af12f1dd877efe7
Anton Älgmyr <anton@algmyr.se>
2023-04-16 00:40:54 +0200

    Colorizer settings

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -2,7 +2,22 @@ return {
   "machakann/vim-highlightedyank",
   {
     "NvChad/nvim-colorizer.lua",
-    opts = {},
+    opts = {
+      filetypes = { "*" },
+      user_default_options = {
+        RGB = true,          -- #RGB hex codes
+        RRGGBB = true,       -- #RRGGBB hex codes
+        names = false,       -- "Name" codes like Blue or blue
+        RRGGBBAA = true,     -- #RRGGBBAA hex codes
+        AARRGGBB = false,    -- 0xAARRGGBB hex codes
+        rgb_fn = true,       -- CSS rgb() and rgba() functions
+        hsl_fn = true,       -- CSS hsl() and hsla() functions
+        mode = "background", -- Mode (foreground, background, virtualtext)
+        virtualtext = "■",
+      },
+      -- all the sub-options of filetypes apply to buftypes
+      buftypes = {},
+    },
   },
   "numirias/semshi",
 }
