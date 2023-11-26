fa7c360ab7bab871c9e6e34cca1ca908b068173c
Anton Älgmyr <anton@algmyr.se>
2023-11-26 15:41:08 +0100

    Add preferred defaults for splice.vim

--- lua/plugins/vcs.lua
+++ lua/plugins/vcs.lua
@@ -1,5 +1,11 @@
 return {
-  "sjl/splice.vim",
+  {
+    "sjl/splice.vim",
+    config = function()
+      vim.g.splice_initial_layout_grid = 1
+      vim.g.splice_initial_diff_grid = 1
+    end,
+  },
   {
     "mhinz/vim-signify",
     build = ":UpdateRemotePlugins",
