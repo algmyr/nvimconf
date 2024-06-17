3aac76388831f846cba0bb99861143c6f61762fe
Anton Älgmyr <anton@algmyr.se>
2024-06-17 18:37:43 +0200

    Configure treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -51,6 +51,17 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
     event = "VeryLazy",
+    config = function()
+      require'treesitter-context'.setup{
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 5, -- Maximum number of lines to show for a single context
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+      }
+    end
   },
 }
 
