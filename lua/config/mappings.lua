dea19c940410b233af8949a1cece28b5c2e2f7d5
Anton Älgmyr <anton@algmyr.se>
2024-07-28 15:47:33 +0200

    Mapping infra revamp.

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,41 +1,42 @@
-require 'mapping'
+local m = require "mapping"
 
-nmap('<leader>/', ':nohlsearch<CR>', 'Unhighlight search')
+m.mappings "signify" {
+  ["<leader>s"] = {
+    f = m.normal { "<cmd>SignifyFold!<CR>", "Fold" },
+    u = m.normal { "<cmd>SignifyHunkUndo<CR>", "Hunk undo" },
+    d = m.normal { "<cmd>SignifyHunkDiff<CR>", "Hunk diff" },
+    D = m.normal { "<cmd>SignifyDiff<CR>", "Diff" },
+    l = m.normal { "<cmd>SignifyList<CR>", "List" },
+    t = m.normal { "<cmd>SignifyToggle<CR>", "Toggle" },
+    h = m.normal { "<cmd>SignifyToggleHighlight<CR>", "Toggle highlight" },
+    r = m.normal { "<cmd>SignifyRefresh<CR>", "Refresh" },
+  },
+}
 
-nmap('<leader>bk', ':bp <BAR> bd #<cr>', 'Kill buffer')
+m.mappings "iswap" {
+  ["<leader>gi"] = m.normal { "<cmd>ISwapWith<CR>", "Swap" },
+  ["<m-left>"] = m.normal { "<cmd>ISwapNodeWithLeft<CR>", "Swap with left" },
+  ["<m-right>"] = m.normal { "<cmd>ISwapNodeWithRight<CR>", "Swap with right" },
+}
 
--- Other mappings
-vmap('<', '<gv', 'Dedent selection')
-vmap('>', '>gv', 'Indent selection')
-nmap('Q', '@q', 'Replay the q macro')
+m.mappings "telescope" {
+  ["<leader>f"] = {
+    f = m.normal { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find files" },
+    g = m.normal { "<cmd>lua require('telescope.builtin').live_grep()<cr>", "Live grep" },
+    b = m.normal { "<cmd>lua require('telescope.builtin').buffers()<cr>", "Buffers" },
+    h = m.normal { "<cmd>lua require('telescope.builtin').help_tags()<cr>", "Help tags" },
+  },
+}
 
-nmap('<leader>sf', ':SignifyFold!<CR>', 'Fold (signify)')
-nmap('<leader>su', ':SignifyHunkUndo<CR>', 'Hunk undo (signify)')
-nmap('<leader>sd', ':SignifyHunkDiff<CR>', 'Hunk diff (signify)')
-nmap('<leader>sD', ':SignifyDiff<CR>', 'Diff (signify)')
-nmap('<leader>sl', ':SignifyList<CR>', 'List (signify)')
-nmap('<leader>st', ':SignifyToggle<CR>', 'Toggle (signify)')
-nmap('<leader>sh', ':SignifyToggleHighlight<CR>', 'Toggle highlight (signify)')
-nmap('<leader>sr', ':SignifyRefresh<CR>', 'Refresh (signify)')
-
-nmap('<leader>gi', ':ISwapWith<CR>', 'Swap (iswap)')
-nmap('<m-left>', ':ISwapNodeWithLeft<CR>', 'Swap with left (iswap)')
-nmap('<m-right>', ':ISwapNodeWithRight<CR>', 'Swap with right (iswap)')
-
-nmap('<leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', 'Find files (telescope)')
-nmap('<leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', 'Live grep (telescope)')
-nmap('<leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', 'Buffers (telescope)')
-nmap('<leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', 'Help tags (telescope)')
-
-nmap('<leader>cd', ':cd %:h<cr>:pwd<cr>', 'cd to current buffer', {silent=false})
----@diagnostic disable-next-line: missing-parameter
-nmap('<space><space>', function() require('notify').dismiss() end, 'Dismiss notification')
-
-vim.api.nvim_create_user_command(
-  'Wikt',
-  function(opts)
-    local url = 'https://' .. opts.args .. '.wiktionary.org/wiki/' .. vim.call('expand','<cword>')
-    io.popen('xdg-open ' .. url)
-  end,
-  { nargs = 1 }
-)
+m.mappings "misc" {
+  ["<leader>"] = {
+    ["/"] = m.normal { "<cmd>nohlsearch<CR>", "Unhighlight search" },
+    ["bk"] = m.normal { "<cmd>bp <BAR> bd #<cr>", "Kill buffer" },
+    ["cd"] = m.normal { "<cmd>cd %:h<cr>:pwd<cr>", "cd to current buffer", { silent = false } },
+  },
+  ["<"] = m.visual { "<gv", "Dedent selection" },
+  [">"] = m.visual { ">gv", "Indent selection" },
+  ["Q"] = m.normal { "@q", "Replay the q macro" },
+  ---@diagnostic disable-next-line: missing-fields
+  ["<space><space>"] = m.normal { function() require("notify").dismiss {} end, "Dismiss notification" },
+}
--- lua/mapping.lua
+++ lua/mapping.lua
@@ -3,9 +3,7 @@ function map(mode, lhs, rhs, desc, opts)
   if opts then
       options = vim.tbl_extend("force", options, opts)
   end
-  --print(mode, lhs, rhs, vim.inspect(options))
   vim.keymap.set(mode, lhs, rhs, options)
-  --vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 function nmap(lhs, rhs, desc, opts)
@@ -32,3 +30,56 @@ function user_command(name, fun, opts)
   -- preview is a thing, interesting
   vim.api.nvim_create_user_command(name, fun, opts or {})
 end
+
+local M = {}
+
+-----------------------------------------------------
+
+-- Convenience functions for creating single model mappings.
+M.normal = function(t)
+  return {normal = t}
+end
+
+M.visual = function(t)
+  return {visual = t}
+end
+
+M.select = function(t)
+  return {select = t}
+end
+
+M.insert = function(t)
+  return {insert = t}
+end
+
+local groupify = function(t, group)
+  if t[2] and group then
+    t[2] = t[2] .. " (" .. group .. ")"
+  end
+  return t
+end
+
+local function make_map(t, key, binding, group)
+  if t[key] then
+    local mode = string.sub(key, 1, 1)
+    map(mode, binding, unpack(groupify(t[key], group)))
+    t[key] = nil
+  end
+end
+
+M.mappings = function(group)
+  return function(binding_table)
+    local function dfs(binding, t)
+      make_map(t, "normal", binding, group)
+      make_map(t, "visual", binding, group)
+      make_map(t, "select", binding, group)
+      make_map(t, "insert", binding, group)
+      for k, v in pairs(t) do
+        dfs(binding .. k, v)
+      end
+    end
+    dfs("", binding_table)
+  end
+end
+
+return M
