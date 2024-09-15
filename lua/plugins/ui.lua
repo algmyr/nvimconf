c176c39b3396f5f03fd4739c7aafe38d29a5113d
Anton Älgmyr <anton@algmyr.se>
2024-09-15 18:55:41 +0200

    UI updates, mainly use the recommended noice config.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -139,13 +139,13 @@ return {
   {
     'folke/noice.nvim',
     config = function()
-      require('noice').setup {
+      require("noice").setup({
         lsp = {
           -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
           override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true,
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
           },
         },
         -- you can enable a preset for easier configuration
@@ -154,13 +154,9 @@ return {
           command_palette = true, -- position the cmdline and popupmenu together
           long_message_to_split = true, -- long messages will be sent to a split
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
+          lsp_doc_border = false, -- add a border to hover docs and signature help
         },
-        -- Classic cmdline
-        cmdline = {
-          view = 'cmdline',
-        },
-      }
+      })
     end,
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
