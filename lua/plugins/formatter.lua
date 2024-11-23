6126623f22f612ea180a6a07cddb22615b55b2bb
Anton Älgmyr <anton@algmyr.se>
2024-11-23 22:08:28 +0100

    Cleanup

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -5,20 +5,6 @@ return {
 
     conform.setup {
       formatters_by_ft = {
-        -- javascript = { "prettier" },
-        -- typescript = { "prettier" },
-        -- javascriptreact = { "prettier" },
-        -- typescriptreact = { "prettier" },
-        -- svelte = { "prettier" },
-        -- css = { "prettier" },
-        -- html = { "prettier" },
-        -- json = { "prettier" },
-        -- yaml = { "prettier" },
-        -- markdown = { "prettier" },
-        -- graphql = { "prettier" },
-        -- lua = { "stylua" },
-        -- python = { "isort", "ruff_format" },
-
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         cs = { 'clang-format' },
@@ -47,49 +33,4 @@ return {
   end,
 }
 
--- return {
---   "mhartington/formatter.nvim",
---   config = function() -- {{{
---     require("formatter").setup {
---       logging = true,
---       log_level = vim.log.levels.WARNING,
---       filetype = {
---         c = require("formatter.filetypes.c").clangformat,
---         cpp = require("formatter.filetypes.c").clangformat,
---         cs = require("formatter.filetypes.cs").clangformat,
---         go = require("formatter.filetypes.go").gofmt,
---         javascript = require("formatter.filetypes.javascript").clangformat,
---         json = require("formatter.filetypes.json").jq,
---         lua = require("formatter.filetypes.lua").stylua,
---         python = {
---           {
---             exe = "ruff format",
---             args = { "-q", "--preview", "-" },
---             stdin = true,
---           },
---           {
---             exe = "ruff check",
---             args = { "--select", "I", "--fix", "-" },
---             stdin = true,
---           },
---         },
---         rust = {
---           function()
---             return {
---               exe = "~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rustfmt",
---               stdin = true,
---             }
---           end,
---         },
---         sh = require("formatter.filetypes.sh").shfmt,
---         typescript = require("formatter.filetypes.typescript").clangformat,
---
---         ["*"] = {
---           require("formatter.filetypes.any").remove_trailing_whitespace,
---         },
---       },
---     }
---   end, -- }}}
--- }
---
 -- -- vim: set fdm=marker:
