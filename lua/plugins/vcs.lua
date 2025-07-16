b60132e281fdb3739c15f7df7d6f8f1156ddb743
Anton Älgmyr <anton@algmyr.se>
2025-07-16 16:47:21 +0200

    vctools: Remove explicit deps in config.

--- lua/plugins/vcs.lua
+++ lua/plugins/vcs.lua
@@ -1,7 +1,6 @@
 return {
   {
     'algmyr/vcsigns.nvim',
-    dependencies = { 'algmyr/vclib.nvim' },
     config = function()
       require('vcsigns').setup {
         target_commit = 1,
@@ -10,7 +9,6 @@ return {
   },
   {
     'algmyr/vcmarkers.nvim',
-    dependencies = { 'algmyr/vclib.nvim' },
     config = function() require('vcmarkers').setup {} end,
   },
   {
