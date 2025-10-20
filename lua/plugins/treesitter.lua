f997e1157d8118ebb692b3b484ca6d4208a3b17e
Anton Älgmyr <anton@algmyr.se>
2025-10-20 21:56:08 +0200

    Ignore noisy missing-fields diagnostics for treesitter.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
     event = 'VeryLazy',
     build = ':TSUpdate',
     config = function() -- {{{
+      ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         highlight = {
           enable = true,
