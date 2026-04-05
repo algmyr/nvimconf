7977006857c5a1067e3fd583116315774ce4bfdd
Anton Älgmyr <anton@algmyr.se>
2026-04-06 01:11:26 +0200

    Make formatter use a list of plugin specs for the sake of consistency.

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,36 +1,38 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    local conform = require 'conform'
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      local conform = require 'conform'
 
-    conform.setup {
-      formatters_by_ft = {
-        c = { 'clang-format' },
-        cpp = { 'clang-format' },
-        cs = { 'clang-format' },
-        go = { 'gofmt' },
-        javascript = { 'clang-format' },
-        json = { 'jq' },
-        lua = { 'stylua' },
-        python = { 'ruff_format' },
-        rust = { 'rustfmt' },
-        sh = { 'shfmt' },
-        typescript = { 'clang-format' },
-      },
-    }
+      conform.setup {
+        formatters_by_ft = {
+          c = { 'clang-format' },
+          cpp = { 'clang-format' },
+          cs = { 'clang-format' },
+          go = { 'gofmt' },
+          javascript = { 'clang-format' },
+          json = { 'jq' },
+          lua = { 'stylua' },
+          python = { 'ruff_format' },
+          rust = { 'rustfmt' },
+          sh = { 'shfmt' },
+          typescript = { 'clang-format' },
+        },
+      }
 
-    vim.api.nvim_create_user_command('Format', function(args)
-      local range = nil
-      if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = {
-          start = { args.line1, 0 },
-          ['end'] = { args.line2, end_line:len() },
-        }
-      end
-      require('conform').format { async = true, lsp_fallback = true, range = range }
-    end, { range = true })
-  end,
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        require('conform').format { async = true, lsp_fallback = true, range = range }
+      end, { range = true })
+    end,
+  },
 }
 
 -- -- vim: set fdm=marker:
